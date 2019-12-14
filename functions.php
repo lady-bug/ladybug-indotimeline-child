5b07780be5948ad54fedee7961a5d11677c5cd7e
lady-bug <valerie@lady-bug.info>
2019-12-14 13:36:41 +0100

    override child styles in ms-separate-style

--- functions.php
+++ functions.php
@@ -4,12 +4,14 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
-         
+
 if ( !function_exists( 'ms_theme_editor_child_css' ) ):
     function ms_theme_editor_child_css() {
         wp_enqueue_style( 'ms_theme_editor_child_ms_separate', trailingslashit( get_stylesheet_directory_uri() ) . 'ms-separate-style.css', array( 'indotimeline-style','indotimeline-build-style','indotimeline-build-icons','indotimeline-custom' ) );
     }
 endif;
+
 add_action( 'wp_enqueue_scripts', 'ms_theme_editor_child_css', 20 );
 
 // END ENQUEUE PARENT ACTION
+?>
