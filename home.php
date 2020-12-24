e73ab7c5ac4a877201f3ff0f995e401e6531b5e9
lady-bug <valerie@lady-bug.info>
2020-12-24 13:33:31 +0100

    feat(home): specific hme template for posts listing

---
+++ home.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package IndoTimeline
+ */
+
+get_header();
+?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<div class="container">
+
+				<div class="main-content">
+
+					<header class="page-header timeline-header">
+						<div class="timeline-header-inner">
+							<?php
+							single_post_title( '<h1 class="page-title">', '</h1>' );
+							?>
+						</div>
+					</header><!-- .page-header -->
+
+					<?php indotimeline_timeline_before() ?>
+						<?php
+						/* Start the Loop */
+						while ( have_posts() ) :
+							the_post();
+
+							/*
+							 * Include the Post-Type-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', get_post_type() );
+
+						endwhile;
+						?>
+
+					<?php indotimeline_timeline_after() ?>
+					<?php the_posts_navigation(); ?>
+				</div>
+				<!-- .main-content -->
+
+				<div class="side-content">
+					<?php get_sidebar(); ?>
+				</div>
+
+			</div>
+			<!-- /.container -->
+
+		<?php else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar('fixed');
+get_footer();
+?>
